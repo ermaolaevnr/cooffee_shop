20f6b2114f9f8d4182363fbbb803ee445d60b020
Ermolaev Nikita <ermolaevnr@yandex.ru>
2025-02-02 20:06:47 +0400

    Обвязка репозитория

---
+++ script.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector('#burger')
+let nav = document.querySelector('#nav')
+burger.addEventListener('click', () =>  {
+  nav.classList.toggle('navigation__active');
+  burger.classList.toggle('header__burger_active');
+})
